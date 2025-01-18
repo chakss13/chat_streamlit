ab5d2d597ec584ccff0f464bd2529c54c10cb92e
chakss13 <157786701+chakss13@users.noreply.github.com>
2025-01-18 17:04:32 +0530

    Update db.py

--- db.py
+++ db.py
@@ -2,22 +2,17 @@ from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
 import gridfs
 
-MONGO_URI = "cluster0-shard-00-00.y2d81.mongodb.net:27017"
+MONGO_URI = "mongodb+srv://<admin>:<admin>@cluster0.y2d81.mongodb.net/messaging_app?retryWrites=true&w=majority"
 
 try:
     client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=5000)
-    client.server_info()
+    client.server_info()  # Force connection on a request as a test
     print("Connected to MongoDB successfully!")
 except ServerSelectionTimeoutError as err:
     print(f"Failed to connect to MongoDB: {err}")
     client = None
 
-if client:
-    db = client['messaging_app']
-    users_collection = db['users']
-    messages_collection = db['messages']
-    fs = gridfs.GridFS(db)
-else:
-    users_collection = None
-    messages_collection = None
-    fs = None
+db = client['messaging_app'] if client else None
+users_collection = db['users'] if db else None
+messages_collection = db['messages'] if db else None
+fs = gridfs.GridFS(db) if db else None
